784e149394de380704c973d8349b32d620384412
Rumen Petrov <rumenn2o@gmail.com>
2014-11-20 15:41:11 +0200

    refactoring js code

--- js/main.js
+++ js/main.js
@@ -4,74 +4,84 @@
     var userChoice;
 
     $doc.ready(function() {
+        // layout change
         $("#btn-play").on('click', function(event) {
             var $this = $(this);
 
             $(".area-head").animate({
                 "top": "15%"
-            }, 200);
+            }, 300);
 
-            $this.parents("li").fadeOut();
-            $this.parents(".area").find(".area-body").fadeIn();
+            $this.parents("li").fadeOut(300);
+            $this.parents(".area").find(".area-body").fadeIn(600);
 
-            $("#user-choice a").on('click', function(event) {
-                var makeChoice;
-                var $this = $(this);
+            event.preventDefault();
+        });
+        
+        // user makes his choice
+        $("#user-choice a").on('click', function(event) {
+            var makeChoice;
+            var $this = $(this);
 
-                $this.addClass("clicked").siblings().removeClass("clicked");
+            $this.addClass("clicked").siblings().removeClass("clicked");
 
-                makeChoice = $this.text().toLowerCase();
+            makeChoice = $this.text().toLowerCase();
 
-                play(makeChoice);
+            // clear result text after every click
+            $(".game-result").text("");
 
-                event.preventDefault();
-            });
+            // execute play function
+            play(makeChoice);
 
             event.preventDefault();
         });
     });
 
-    // play function
+    // function that make computer's choice, compare it with user's choice and print result
     var play = function(userChoice) {
-        setTimeout(function() {
-            var computerChoice = Math.random();
+        var computerChoice = Math.random();
 
-            if (computerChoice < 0.34) {
-                computerChoice = "rock";
-            } else if(computerChoice <= 0.67) {
-                computerChoice = "paper";
-            } else {
-                computerChoice = "scissors";
-            };
-
-            $(".player-first").text("You Choose: " + userChoice);
-            $(".player-second").text("Computer Choose: " + computerChoice);
-
-            var compare = function(choice1, choice2) {
-                if ( choice1 === choice2) {
-                    $(".game-result").text("The result is a tie!");
-                } else if (choice1 === "rock") {
-                    if (choice2 === "scissors") {
-                        $(".game-result").text("rock wins");
-                    } else if (choice2 === "paper") {
-                        $(".game-result").text("paper wins");
-                    }
-                } else if ( choice1 === "paper") {
-                    if (choice2 === "rock") {
-                        $(".game-result").text("paper wins");
-                    } else if (choice2 === "scissors") {
-                        $(".game-result").text("scissors wins");
-                    }
-                } else if ( choice1 === "scissors") {
-                    if (choice2 === "rock") {
-                        $(".game-result").text("rock wins");
-                    } else  if (choice2 === "paper") {
-                        $(".game-result").text("scissors wins");
-                    }
+        // function that compare choices and print result
+        var compare = function(userChoice, computerChoice) {
+            if ( userChoice === computerChoice) {
+                $(".game-result").text("The result is a tie!");
+            } else if (userChoice === "rock") {
+                if (computerChoice === "scissors") {
+                    $(".game-result").text("you win");
+                } else if (computerChoice === "paper") {
+                    $(".game-result").text("computer win");
+                }
+            } else if ( userChoice === "paper") {
+                if (computerChoice === "rock") {
+                    $(".game-result").text("you win");
+                } else if (computerChoice === "scissors") {
+                    $(".game-result").text("computer win");
+                }
+            } else if ( userChoice === "scissors") {
+                if (computerChoice === "rock") {
+                    $(".game-result").text("computer win");
+                } else  if (computerChoice === "paper") {
+                    $(".game-result").text("you win");
                 }
             }
+        };
 
-            compare(userChoice, computerChoice);
-        }, 500);
+        // computer makes his choice
+        if (computerChoice < 0.20) {
+            computerChoice = "rock";
+        } else if(computerChoice <= 0.6) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        };
+
+        // print out the choices
+        $(".player-first").text("You Choose: " + userChoice);
+        $(".player-second").text("Computer Choose: " + computerChoice);
+
+        // execute compare function with delay
+        setTimeout(function() {
+            compare(userChoice, computerChoice)
+        }, 400);
     };
 })(jQuery, window, document);
